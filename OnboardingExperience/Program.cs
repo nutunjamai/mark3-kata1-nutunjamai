271aaf8f1dcd6d54b759a614254bfa917e436d9d
Skylar Carter <carter.skylar@gmail.com>
2018-04-12 13:07:42 -0500

    trying to figure out bool method.  so stumped and confused

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -7,22 +7,55 @@ namespace OnboardingExperience
         static void Main(string[] args)
         {
             var user = new User();
-            
-            Console.WriteLine("Welcome to Amar Taka, enter your information to setup a new account");
-            Console.WriteLine("\nEnter your First Name");
-            user.FirstName = Console.ReadLine();
-            Console.WriteLine($"\nHi, {user.FirstName} what is your last name");
-            Console.WriteLine("\nEnter Last Name");
-            user.LastName = Console.ReadLine();
-            
-            Console.WriteLine($"\nHi, {user.FirstName} {user.LastName} what is your Pin Number");
-            
-             
-            
-            user.PinNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{user.PinNumber}");
 
+            user.AccountOwner = Accbool("Are you an account owner?");
+
+            Console.WriteLine("Welcome to Amar Taka, enter your information to setup a new account");
+
+
+            user.FirstName = AskQuestion("\nEnter your First Name");
+            Console.WriteLine($"\nHi {user.FirstName},");
+
+
+            user.LastName = AskQuestion("What is your last name");
+            Console.WriteLine($"\nOk {user.FirstName} {user.LastName}");
+
+            user.PinNumber = IntQuestion("what is your Pin Number");
+
+            Console.WriteLine($"{user.PinNumber} Great save your pin and don't share it with anybody");
             Console.ReadKey(true);
+
+        }
+
+        public static int IntQuestion(string questionInt)
+        {
+            Console.WriteLine(questionInt);
+            int num;
+            while (int.TryParse(Console.ReadLine(), out num) == false)
+            {
+                Console.WriteLine("**ERROR** Invalid input enter valid number");
+            }
+            return num;
+        }
+
+        public static bool Accbool(string question)
+        {
+            do
+            {
+                var result = AskQuestion(question + " (y/n)");
+
+                if (result.ToLower() == "y") { return true; }
+                else if (result.ToLower() == "n") { return false; }
+
+                Console.WriteLine("\nYou must enter y or n");
+            } while (true);
+        }
+
+
+        static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
 
 
         }
